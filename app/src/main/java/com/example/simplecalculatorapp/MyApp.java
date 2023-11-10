b1d00cdbf483e63eccca304cd4f458b784ce0648
Participant <bure.rajeswari@gmail.com>
2023-11-10 12:45:12 -0500

    Simple Calculator_Working version -- updated landscape mode

---
+++ app/src/main/java/com/example/simplecalculatorapp/MyApp.java
@@ -0,0 +1,10 @@
+package com.example.simplecalculatorapp;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+
+    String historyList = "";
+
+    boolean isAdvanceHistoryFlag;
+}
